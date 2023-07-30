438db44f7b85dbecc70582a9686807ae8a784b76
mkmahmud <mahmudulhasanmk434@gmail.com>
2023-07-30 12:04:46 +0600

    set success alert

--- src/pages/pcbuild.js
+++ src/pages/pcbuild.js
@@ -2,7 +2,7 @@ import RootLayout from "@/component/layout/RootLayout";
 import Image from "next/image";
 import React from "react";
 import logo from "../assets/logo.png";
-import { Button } from "antd";
+import { Button, message } from "antd";
 import PcBuilderComponent from "@/component/PcBuilderComponent/PcBuilderComponent";
 import { useGetAllcategoriesQuery } from "@/redux/features/category/categoryApi";
 import { useAppSelector } from "@/redux/hook";
@@ -12,27 +12,27 @@ const PcBuild = () => {
     useGetAllcategoriesQuery(undefined);
 
   const pcComponents = useAppSelector((state) => state.pcComponent);
-  console.log();
+  const [messageApi, contextHolder] = message.useMessage();
+  const info = () => {
+    messageApi.info("Your build success");
+  };
 
   return (
     <div className="pcBuild bg-white max-w-screen-lg m-2 p-4 rounded mx-auto text-black">
       <div>
         <Image src={logo} alt={logo} height={80} width={100}></Image>
       </div>
+      {contextHolder}
       <div className="flex justify-between items-center my-2">
         <p>PC Builder - Build your own PC - New Egg</p>
 
         {pcComponents?.pcComponents?.length == 7 ? (
-          <Button type="primary">Build your PC</Button>
+          <Button type="primary" onClick={info}>Build your PC</Button>
         ) : (
           <Button type="primary" disabled>
             Build your PC
           </Button>
         )}
-
-        <Button>
-          Total Ammount $<span>00</span>
-        </Button>
       </div>
       <div>
         <h4 className="p-2 bg-gray-200 ">CORE COMPONENTS</h4>
